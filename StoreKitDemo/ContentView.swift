46b9ab29894055bd19562210a7ecd873628880a0
Dylan CAETANO <dcaetano8@icloud.com>
2026-01-05 23:06:29 +0100

    Initial Commit

---
+++ StoreKitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoreKitDemo
+//
+//  Created by Dylan caetano on 05/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoreKitDemo/StoreKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKitDemoApp.swift
+//  StoreKitDemo
+//
+//  Created by Dylan caetano on 05/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct StoreKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
